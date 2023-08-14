a24e641fa9f02d26943f4f2f1b99f434f519989b
erick_palacios@usmp.pe <erick_palacios@usmp.pe>
2023-08-14 13:32:26 -0500

    node2

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-import express from "express";
-const app = express()
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
-app.listen(3000)
-console.log('Server on port', 3000)
+const { exec } = require('child_process');
+
+// Ruta al archivo Startup.cs
+const startupPath = '/Startup.cs';  // Cambia esto a la ruta correcta
+
+// Comando para ejecutar el archivo Startup.cs usando dotnet
+const dotnetCommand = `dotnet run ${startupPath}`;
+
+// Ejecutar el comando
+exec(dotnetCommand, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error al ejecutar dotnet: ${error}`);
+    return;
+  }
+  
+  console.log(`Salida de dotnet:\n${stdout}`);
+});
